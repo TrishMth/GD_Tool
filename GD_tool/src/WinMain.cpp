e977a407835f20a408bd78b4777382e0cb6efa29
Unknown <K.schaefer92@gmx.de>
2018-01-20 12:36:58 +0100

    Added Documentation and WinMain
    The Coding Convention Document is added in the docs directory and the WinMain in src directory

---
+++ GD_tool/src/WinMain.cpp
@@ -0,0 +1,29 @@
+#pragma region Internal Includes
+#pragma endregion 
+#pragma region External Includes
+#include <Windows.h>
+#include <fstream>
+#pragma endregion 
+///
+///\brief
+///
+///Main function 
+///
+/// 
+///<CENTER>
+///<param name= "hInstacne"> Handle of the current instance of the application. 
+///<param name="hPrevInstance"> Handle of the previous Instance of the application. Use this to check if other instances of the application are open. 
+///<param name="lpCmdLine"> The command line for the application excluding the program name. 
+///<param name="nCmdShow"> Controls how the window is to be shown 
+///<DL>
+///<LI> SW_HIDE (0) = Hides the window and activates another window </LI>
+///<LI> SW_MAXIMIZE (3) = Maximizes the specified Window.  </LI>
+///<LI> SW_MINIMIZE (6) = Minimizes the specified window and activates the next top-level window in the Z-order. </LI>
+///</DL>
+///</CENTER>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	return 1; 
+}
